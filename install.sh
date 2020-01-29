052664dea942735b9c5f21bef968af94dbb9e556
Miguel Mota <hello@miguelmota.com>
2020-01-29 08:57:14 -0800

    Update i3config

--- install.sh
+++ install.sh
@@ -3,97 +3,127 @@
 unamestr=$(uname)
 
 filesToLink=(
-	"aliases"
-	"ackrc"
-	"apache2"
+  "aliases"
+  "ackrc"
+  "agignore"
+  "apache2"
 
-	"bash_aliases"
-	"bash_profile"
-	"bashrc"
+  "bash_aliases"
+  "bash_profile"
+  "bashrc"
 
-	"bin"
-	#"coffeelint.json"
-	#"config"
-	"cortex"
-	"dircolors"
-	"docker"
-	"elinks"
-	"emacs.d"
-	"filezilla"
-	"functions"
-	"fonts"
+  "bin"
+  "coffeelint.json"
+  "config/alacritty/alacritty.yml"
+  "config/fontconfig/conf.d/10-powerline-symbols.conf"
+  "config/i3/config"
+  "config/i3/workspace-1.json"
+  "config/i3/workspace-2.json"
+  "config/kitty/kitty.conf"
+  "config/konsolerc"
+  "config/lxterminal/lxterminal.conf"
+  "config/neofetch/config.conf"
+  "config/pcmanfm/default/pcmanfm.conf"
+  "config/pcmanfm/lubuntu/desktop-items-0.conf"
+  "config/pcmanfm/lubuntu/pcmanfm.conf"
+  "config/pcmanfm/LXDE/desktop-items-0.conf"
+  "config/pcmanfm/LXDE/pcmanfm.conf"
+  "config/powerline/colors.json"
+  "config/powerline/colorschemes"
+  "config/powerline/config.json"
+  "config/powerline/themes"
+  "config/qterminal.org/qterminal.ini"
+  "config/rofi/config"
+  "config/rofi/themes"
+  "config/terminator/config"
+  "cortex"
+  "dircolors"
+  "docker"
+  "elinks"
+  "emacs.d"
+  "exports"
+  "filezilla"
+  "functions"
+  "fonts"
 
-	"gcalcli"
-	"gemrc"
-	"ghci"
-	"gitattributes"
-	"gitconfig"
-	"gitmodules"
-	"gitradarrc"
-	"gitignore"
+  "gcalcli"
+  "gemrc"
+  "ghci"
+  "gitattributes"
+  "gitconfig"
+  "gitmodules"
+  "gitradarrc"
+  "gitignore"
 
-	"httpie"
-	"hushlogin"
-	"irssi"
-	"jupyter"
-	"mc"
-	"mongo"
-	"mutt"
-	"muttrc"
-	"NERDTreeBookmarks"
-	"powerline"
-	"profile"
-	"promptrc"
-	"pyhn"
-	"redis"
-	"s3cfg"
-	#"ssh"
+  "httpie"
+  "hushlogin"
+  "irssi"
+  "jupyter"
+  "local/share/konsole/Main.profile"
+  "local/share/konsole/Shapeshifter.colorscheme"
+  "mc"
+  "mongo"
+  "mutt"
+  "muttrc"
+  "NERDTreeBookmarks"
+  "powerline"
+  "powerline-shell"
+  "profile"
+  "promptrc"
+  "pyhn"
+  "redis"
+  "s3cfg"
+  #"ssh"
+  "terminal"
+  "termite"
+  "tern-config"
+  "themes"
+  "tmux"
+  "tmux.conf"
+  "tmuxinator"
+  #"twitter-alarm"
 
-	"tern-config"
-	"themes"
-	"tmux"
-	"tmux.conf"
-	"tmuxinator"
-	#"twitter-alarm"
+  "urxvt"
 
-	"urxvt"
+  #"vim"
+  #"vimrc"
+  #viminfo"
 
-	#"vim"
-	#"vimrc"
-	#viminfo"
+  "wegorc"
+  "w3m"
+  "weinre"
 
-	"wegorc"
-	"w3m"
-	"weinre"
+  "Xresources"
+  "Xdefaults"
 
-	"Xresources"
-	"Xdefaults"
+  "oh-my-zsh"
 
-	"oh-my-zsh"
+  "z"
+  "z.sh"
+  "zshrc"
+  "zshrc-e"
+  "zsh-update"
 
-	"zshrc"
-	"zshrc-e"
-	"zsh-update"
-
-	"zprezto"
-	"zpreztorc"
-	"zlogin"
-	"zlogout"
-	"zshenv"
-	"zprofile"
+  "zprezto"
+  "zpreztorc"
+  "zlogin"
+  "zlogout"
+  "zshenv"
+  "zprofile"
 )
 
 for f in ${filesToLink[@]}; do
-	if [ -d ~/.$f ] || [ -f ~/.$f ]; then
-		echo "~/.$f exists; skipping."
-	else
-		if [ -d $f ] || [ -f $f ]; then
-			echo "symlinking ~/Dotfiles/dotfiles/$f -> ~/.$f"
-			ln -snf ~/Dotfiles/dotfiles/$f ~/.$f
-		else
-			echo "$f not found; skipping."
-		fi
-	fi
+  if [ -d ~/.$f ] || [ -f ~/.$f ]; then
+    echo "~/.$f exists; skipping."
+  else
+    if [ -d $f ] || [ -f $f ]; then
+      echo "symlinking ~/Dotfiles/dotfiles/$f -> ~/.$f"
+      mkdir -p ~/.$(dirname $f)
+      ln -snf ~/Dotfiles/dotfiles/$f ~/.$f
+    else
+      echo "$f not found; skipping."
+    fi
+  fi
 done
 
 #ln -snf ~/Dotfiles/dotfiles/aliases.local ~/.aliases.local
@@ -103,14 +133,14 @@ done
 #sudo ln -snf ~/Dropbox/.apache2/users/moogs.conf /etc/apache2/users/moogs.conf
 
 if [[ "$unamestr" == 'Darwin' ]]; then
-	echo "symlinking ~/Dotfiles/dotfiles/homebrew -> ~/.homebrew"
-	ln -snf ~/Dotfiles/dotfiles/homebrew ~/.homebrew
+  echo "symlinking ~/Dotfiles/dotfiles/homebrew -> ~/.homebrew"
+  ln -snf ~/Dotfiles/dotfiles/homebrew ~/.homebrew
 fi
 
 
 #ln -snf ~/Dotfiles/dotfiles/webstorm ~/.webstorm
 #if [[ "$unamestr" == 'Darwin' ]]; then
-	#ln -snf ~/Dropbox/.webstorm/colors /Users/moogs/Library/Preferences/WebStorm7/colors
+#ln -snf ~/Dropbox/.webstorm/colors /Users/moogs/Library/Preferences/WebStorm7/colors
 #fi
 
 # ln -snf ~/Dotfiles/dotfiles/stickies/StickiesDatabase ~/Library/StickiesDatabase
@@ -120,49 +150,72 @@ ln -snf ~/Dotfiles/dotfiles/z/z.sh ~/.z.sh
 
 # Packages
 if [[ "$unamestr" == 'Darwin' ]]; then
-	# TODO install brew if not installed
+  which -s brew
+  if [[ $? != 0 ]] ; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 
-	brew install vim
-	brew install tmux
-	brew install wget
-	brew install michaeldfallen/formula/git-radar
-	brew install tree
-	brew install fortune
-	brew install reattach-to-user-namespace
-	brew install hub
-	brew install python # installs pip
-	pip install powerline
-else
-	if [ -f "/etc/debian_version" ]; then
-		sudo apt-get update -y
-		sudo apt-get install git -y
-		sudo apt-get install vim -y
-		sudo apt-get install tmux -y
-		sudo apt-get install wget -y
-		sudo apt-get install curl -y
-		sudo apt-get install make -y
-		sudo apt-get install cmake -y
-		sudo apt-get install build-essential -y
-		sudo apt install gnome-tweak-tool -y
-		sudo apt-get install dconf-tools -y
-		sudo apt-get install xclip -y
-		sudo apt-get install net-tools -y
-		sudo apt-get install nmap -y
-	  	sudo apt-get install font-manager -y
-	fi
+  brew install vim
+  brew install tmux
+  brew install wget
+  brew install michaeldfallen/formula/git-radar
+  brew install tree
+  brew install fortune
+  brew install reattach-to-user-namespace
+  brew install hub
+  brew install python # installs pip
+  pip install powerline
+fi
+
+if [ -f "/etc/debian_version" ]; then
+  sudo apt-get update -y
+  sudo apt-get install -y git
+  sudo apt-get install -y vim
+  sudo apt-get install -y tmux
+  sudo apt-get install -y wget
+  sudo apt-get install -y curl
+  sudo apt-get install -y make
+  sudo apt-get install -y cmake
+  sudo apt-get install -y build-essential
+  sudo apt-get install -y gnome-tweak-tool
+  sudo apt-get install -y dconf-tools
+  sudo apt-get install -y xclip
+  sudo apt-get install -y net-tools
+  sudo apt-get install -y nmap
+  sudo apt-get install -y gnome-screenshot
+  sudo apt-get install -y font-manager
+  sudo apt-get install -y neofetch
+fi
+
+if grep NAME=Fedora /etc/os-release; then
+  sudo dnf install -y git
+  sudo dnf install -y vim
+  sudo dnf install -y tmux
+  sudo dnf install -y wget
+  sudo dnf install -y curl
+  sudo dnf install -y xclip
+  sudo dnf install -y tree
+  sudo dnf install -y nmap
+  sudo dnf install -y gnome-screenshot
+  sudo dnf install -y make automake gcc gcc-c++
+  sudo dnf install -y light
+  sudo dnf install -y neofetch
+
+  dnf copr enable gregw/i3desktop
+  sudo dnf install i3-gaps --allowerasing
 fi
 
 echo "installing fonts"
 if [[ "$unamestr" == 'Darwin' ]]; then
-	(cd ./fonts && ./install)
+  (cd ./fonts && ./install)
 else
-	fc-cache -f -v ~/.fonts
+  fc-cache -f -v ~/.fonts
 fi
 
 if [ ! -f $HOME/.tmux/plugins/tpm/bin/install_plugins ]; then
-	echo "Installing ~/.tmux/plugins/tpm"
-	rm -rf $HOME/.tmux/plugins/*
-	git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+  echo "Installing ~/.tmux/plugins/tpm"
+  rm -rf $HOME/.tmux/plugins/*
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
 
 # For powerline
@@ -170,9 +223,9 @@ fi
 #pip install --upgrade pip
 
 if [ ! -d "/usr/lib/urxvt/perl" ]; then
-	echo "Copying urxvt perls"
-	sudo mkdir -p /usr/lib/urxvt/perl
-	sudo cp urxvt/ext/* /usr/lib/urxvt/perl/
+  echo "Copying urxvt perls"
+  sudo mkdir -p /usr/lib/urxvt/perl
+  sudo cp urxvt/ext/* /usr/lib/urxvt/perl/
 fi
 
 if [ ! -f "$HOME/.local/share/konsole/Main.profile" ]; then
@@ -187,11 +240,11 @@ source ~/.bashrc
 
 if [ ! -f "$TMUX_PLUGIN_MANAGER_PATH/tmux-mem-cpu-load/tmux-mem-cpu-load" ]; then
   (
-    echo "installing tmux plugin dependencies"
-    cd $TMUX_PLUGIN_MANAGER_PATH/tmux-mem-cpu-load
-    cmake .
-    make
-  )
+  echo "installing tmux plugin dependencies"
+  cd $TMUX_PLUGIN_MANAGER_PATH/tmux-mem-cpu-load
+  cmake .
+  make
+)
 fi
 
 echo "done."
